3a126f4810ea6f863c7bbebf243d2d66d0ab9c50
carolinedeng <caroline.m.deng@gmail.com>
2017-04-13 17:51:48 -0400

    preliminary keyboard functionality
    added frequency array and function to play a note if the canvas is
    clicked

--- public/script.js
+++ public/script.js
@@ -1,17 +1,37 @@
-var song;
+var notes = [ 220, 245, 260, 280, 300, 320, 340, 360];
+
+var attackLevel = 1.0;
+var releaseLevel = 0;
+
+var attackTime = 0.001
+var decayTime = 0.2;
+var susPercent = 0.2;
+var releaseTime = 0.5;
+
+var env, triOsc;
 
 function setup() {
-  song = loadSound('song.mp3');
-  createCanvas(720, 200);
-  background(255,0,0);
+  var cnv = createCanvas(800, 400);
+  background(100);
+
+  env = new p5.Env();
+  env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+  env.setRange(attackLevel, releaseLevel);
+
+  triOsc = new p5.Oscillator('triangle');
+  triOsc.amp(env);
+  triOsc.start();
+  triOsc.freq(notes[1]);
+
+  cnv.mousePressed(playEnv);
 }
 
-function mousePressed() {
-  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.stop();
-    background(255,0,0);
-  } else {
-    song.play();
-    background(0,255,0);
-  }
+function playEnv(){
+  // trigger env on triOsc, 0 seconds from now
+  // After decay, sustain for 0.2 seconds before release
+  env.play(triOsc, 0, susPercent);
+}
+
+function draw() {
+  fill(0);
 }
