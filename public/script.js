a47e20bf4b19e7de11256fc84861fb0c842a7b97
carolinedeng <caroline.m.deng@gmail.com>
2017-04-15 21:40:10 -0400

    moved images into the public folder for Heroku
    moved images to be displayable on Heroku

--- public/script.js
+++ public/script.js
@@ -4,17 +4,17 @@ buzz.defaults.preload = 'metadata';
 
 var keys = [  
 // first note is C, and then C sharp, and so on.
-  { img: '../images/1.png' , sound: '036' },
-  { img: '../images/2.png' , sound: '037' },
-    { img: '../images/3.png', sound: '038' },
-  { img: '../images/4.png' , sound: '039' },  
-  { img: '../images/5.png' , sound: '040' },
-  { img: '../images/6.png' , sound: '041' },
-  { img: '../images/7.png' , sound: '042' },
-  { img: '../images/8.png' , sound: '043' },
-  { img: '../images/9.png' , sound: '044' },
-  { img: '../images/10.png' , sound: '045' },
-  { img: '../images/11.png' , sound: '046' },
+  { img: 'images/1.png' , sound: '036' },
+  { img: 'images/2.png' , sound: '037' },
+    { img: 'images/3.png', sound: '038' },
+  { img: 'images/4.png' , sound: '039' },  
+  { img: 'images/5.png' , sound: '040' },
+  { img: 'images/6.png' , sound: '041' },
+  { img: 'images/7.png' , sound: '042' },
+  { img: 'images/8.png' , sound: '043' },
+  { img: 'images/9.png' , sound: '044' },
+  { img: 'images/10.png' , sound: '045' },
+  { img: 'images/11.png' , sound: '046' },
   { img: 'b.png' , sound: '047' },
   { img: 'c.png', sound: '048'}
 ];                                                                            
