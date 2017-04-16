0cb6154d4c0c683dfb6992dc52604a3150d0b809
carolinedeng <caroline.m.deng@gmail.com>
2017-04-15 21:33:33 -0400

    got the piano keys to work!
    Adapted PracticeEngineer.com’s code from his YouTube tutorial

--- public/script.js
+++ public/script.js
@@ -1,85 +1,54 @@
-var notes = [ 220, 245, 260, 280, 300, 320, 340, 360];
+ 
+buzz.defaults.formats = [ 'wav' ]; 
+buzz.defaults.preload = 'metadata'; 
 
-var attackLevel = 1.0;
-var releaseLevel = 0;
+var keys = [  
+// first note is C, and then C sharp, and so on.
+  { img: '../images/1.png' , sound: '036' },
+  { img: '../images/2.png' , sound: '037' },
+    { img: '../images/3.png', sound: '038' },
+  { img: '../images/4.png' , sound: '039' },  
+  { img: '../images/5.png' , sound: '040' },
+  { img: '../images/6.png' , sound: '041' },
+  { img: '../images/7.png' , sound: '042' },
+  { img: '../images/8.png' , sound: '043' },
+  { img: '../images/9.png' , sound: '044' },
+  { img: '../images/10.png' , sound: '045' },
+  { img: '../images/11.png' , sound: '046' },
+  { img: 'b.png' , sound: '047' },
+  { img: 'c.png', sound: '048'}
+];                                                                            
 
-var attackTime = 0.001
-var decayTime = 0.2;
-var susPercent = 0.2;
-var releaseTime = 0.5;
 
-var env, triOsc;
+$( function() {
+    if ( !buzz.isSupported() ) {
+        $('#warning').show(); 
+    }
 
-var c;
+    // identify variables/ associate with a specific element
+    var idx = 0,
+        $container  = $( '#container' ),
+        $sent    = $( '#sent' );
 
-function setup() {
-  var cnv = createCanvas(800, 400);
-  background(255);
 
-  env = new p5.Env();
-  env.setADSR(attackTime, decayTime, susPercent, releaseTime);
-  env.setRange(attackLevel, releaseLevel);
+    function buildPiano() {
+ 
+        // Build model  
+        var fLen = keys.length; 
+     
+    for (i = 0; i < fLen; i++) {
+      var letter = keys[i].sound;
+      $sent.append( '<img id="' + letter + '" src=' + keys[i].img + ' alt="' + letter + '">');
+    }
+    
+    $( 'img' ).mousedown( function() {
+        var id = $(this).attr('alt');
+        var snckSndsply = new buzz.sound(id);
+        snckSndsply.play();
+            
+            });
+        
+    }
 
-  triOsc = new p5.Oscillator('triangle');
-  triOsc.amp(env);
-  triOsc.start();
-  triOsc.freq(notes[1]);
-
-  cnv.mousePressed(playEnv);
-
-}
-
-function playEnv(){
-  // trigger env on triOsc, 0 seconds from now
-  // After decay, sustain for 0.2 seconds before release
-  env.play(triOsc, 0, susPercent);
-}
-
-function draw() {
-  fill(180, 214, 237);
-  noStroke();
-
-  c = color(50, 55, 100);
-  fill(252, 171, 185);
-  rect(0, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(234, 121, 140);
-  rect(70, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(226, 49, 79);
-  rect(140, 10, 60, 800);
-
-  c= color(50, 55, 100);
-  fill(232, 161, 120);
-  rect(210, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(242, 232, 137);
-  rect(280, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(135, 219, 129);
-  rect(350, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(120, 187, 232);
-  rect(420, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(120, 163, 232);
-  rect(490, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(109, 127, 219);
-  rect(560, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(87, 83, 193);
-  rect(630, 10, 60, 800);
-
-  c = color(50, 55, 100);
-  fill(55, 52, 153);
-  rect(700, 10, 60, 800);  
-}
+    buildPiano( idx );
+});
